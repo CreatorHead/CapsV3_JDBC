04d9d54dcc85947ad7526ee8b97c8f1249ea20e6
creatorHead <droidbit1@gmail.com>
2018-08-10 11:19:36 +0530

    Added all the Overloaded getConnection() methods.

--- src/com/caps/jsp/MyFirstJDBCProgram.java
+++ src/com/caps/jsp/MyFirstJDBCProgram.java
@@ -1,15 +1,16 @@
 package com.caps.jsp;
 
 
-import java.io.*;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+import java.util.Scanner;
 
 import com.mysql.jdbc.Driver; //required for jdbc to connect to mySql database
-
 
 
 public class MyFirstJDBCProgram {
@@ -26,13 +27,80 @@ public class MyFirstJDBCProgram {
 			java.sql.Driver driverRef = new Driver();
 			DriverManager.registerDriver(driverRef);
 			System.out.println("Driver Loaded...");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			/*
 			 * 2. Get the DB Connection via Driver
 			 */
-						String dbUrl="jdbc:mysql://localhost:3306/capsV3_db"
-								+ "?user=root&password=root";
-			con = DriverManager.getConnection(dbUrl);
+//						String dbUrl="jdbc:mysql://localhost:3306/capsV3_db"
+//								+ "?user=root&password=root";
+			String dbUrl="jdbc:mysql://localhost:3306/capsV3_db";
+//			con = DriverManager.getConnection(dbUrl); //1st version of getConnection
+
+			//2nd Version of getConnection
+//			Scanner in = new Scanner(System.in);
+//			String user = in.nextLine();
+//			String password = in.nextLine();
+//			con = DriverManager.getConnection(dbUrl, user, password);
+			
+			//3rd Way to get a DB Connection
+			String filePath = "F:/Files/db.properties";
+			FileReader reader = new FileReader(filePath);
+			Properties prop = new Properties();
+			prop.load(reader);
+			
+			con = DriverManager.getConnection(dbUrl, prop);
+			
+			
 			System.out.println("Connected...");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			/*
 			 * 3. Issue the SQL query via connection
 			 */
@@ -61,7 +129,7 @@ public class MyFirstJDBCProgram {
 				System.out.println("*********************");
 			}
 
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}finally{
 			/*
